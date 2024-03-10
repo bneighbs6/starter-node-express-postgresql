2147aeaec7fdb2b1d21deb0f4b7460b7a36e1219
Brett <brett_neighbors@yahoo.com>
2024-03-10 15:17:04 -0600

    Updated 4 seed files

--- src/db/seeds/02-categories.js
+++ src/db/seeds/02-categories.js
@@ -1,13 +1,13 @@
+const categories = require("../fixtures/categories");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+exports.seed = function(knex) {
+  return knex
+  .raw("TRUNCATE TABLE categories RESTART IDENTITY CASCADE")
+  .then(function () {
+    return knex("categories").insert(categories)
+  })
 };
