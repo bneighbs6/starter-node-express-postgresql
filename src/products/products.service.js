0fcf735ae8918bb17d8682827567c7ebf818035e
Brett <brett_neighbors@yahoo.com>
2024-03-10 16:13:29 -0600

    Added read() to products.service.js

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,10 +1,20 @@
 const knex = require("../db/connection");
 
+// Creates a knex query
+// selects all columns from products table
+// where product_id mathces argument passed to read()
+// first() returns the first row int he table as an object
+
+function read(productId) {
+  return knex("products").select("*").where({ product_id: productId}).first();
+}
+
 // SQL Query using Knex syntax
 function list() {
   return knex("products").select("*");
 }
 
 module.exports = {
+  read,
   list,
 };
