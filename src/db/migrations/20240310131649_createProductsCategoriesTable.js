97cdb07c7df26675a539b08e3eff5e85636ed813
Brett <brett_neighbors@yahoo.com>
2024-03-10 07:28:35 -0600

    Updated produts categories migration file

--- src/db/migrations/20240310131649_createProductsCategoriesTable.js
+++ src/db/migrations/20240310131649_createProductsCategoriesTable.js
@@ -1,9 +1,25 @@
+const { TableBuilder } = require("knex");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("products_categories", (table) => {
+    table.integer("product_id").unsigned().notNullable();
+    table
+        .foreign("product_id")
+        .references("product_id")
+        .inTable("products")
+        .onDelete("cascade");
+    table.integer("category_id").unsigned().notNullable();
+    table
+        .foreign("category_id")
+        .references("category_id")
+        .inTable("categories")
+        .onDelete("cascade")
+    table.timestamps(true, true);
+  });
 };
 
 /**
@@ -11,5 +27,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable("product_categories")
 };
