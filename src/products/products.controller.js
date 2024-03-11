53877ad5f07e913447c4ff62a75ca087c3e5b732
Brett <brett_neighbors@yahoo.com>
2024-03-11 05:46:05 -0600

    Update productExists to an async function

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,17 +1,15 @@
 const productsService = require("./products.service");
 
 // Validation Middleware
-function productExists(req, res, next) {
-  productsService
-    .read(req.params.productId)
-    .then((product) => {
-      if (product) {
-        res.locals.product = product;
-        return next();
-      }
-      next({status: 404, message: `Product cannot be found.`});
-    })
-    .catch(next);
+
+async function productExists(req, res) {
+  const product = await productsService.read(req.params.productId);
+  if (product) {
+    res.locals.product = product; 
+    return next();
+  }
+  next({status:404, message: `Product cannot be found`})
+
 }
 
 function read(req, res, next) {
